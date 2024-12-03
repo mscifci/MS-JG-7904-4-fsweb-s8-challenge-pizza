a38451e70cada95e9579387e1f830f09966379f9
Mustafa Çifci <ms.cifci@outlook.com>
2024-12-03 16:50:16 +0300

    2 HomePage

---
+++ src/HomePage.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './HomePage.css'; 
+import logo from './assets/logo.svg'; 
+import backgroundImage from './assets/home-banner.png'; 
+
+function HomePage() {
+    return (
+      <div className="homepage-wrapper"> 
+        <div className="homepage" style={{ backgroundImage: `url(${backgroundImage})` }}>
+          <div className="homepage-content"> 
+            <img src={logo} className="homepage-logo" alt="Logo" />
+            <h1 className="homepage-title">
+            <span className="kod">KOD</span>
+              <span> ACIKTIRIR</span>
+              <span> </span>
+              <span className="pizza-doyurur">PİZZA,DOYURUR</span>
+            </h1>
+            <button className="homepage-button">ACIKTIM</button>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default HomePage;
